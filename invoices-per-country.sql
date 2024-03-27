5c9b5bf51efa9af403756cb0578363954a4bf0d9
Alex Sylvain Luenga <38809975+sylvainluenga@users.noreply.github.com>
2024-03-27 13:20:15 +0200

    Update invoices-per-country.sql
    The previous code was fine but was still an error on line 2 and 4 , That's why i deceided to join the from

--- invoices-per-country.sql
+++ invoices-per-country.sql
@@ -3,3 +3,14 @@ FROM country co, city ci, customer cu, invoice i
 WHERE co.id = ci.country_id AND ci.id = cu.city_id AND cu.id = i.customer_id
 GROUP BY co.country name
 HAVING AVG(i.total_price) > (SELECT AVG(total price))
+
+
+==========================================================or this will work perfectly as i just did the test myself now ========================
+SELECT co.country_name, COUNT(*), AVG(i.total_price)
+FROM country co
+JOIN city ci ON co.id = ci.country_id
+JOIN customer cu ON ci.id = cu.city_id
+JOIN invoice i ON cu.id = i.customer_id
+GROUP BY co.country_name
+HAVING AVG(i.total_price) > (SELECT AVG(total_price) FROM invoice);
+
