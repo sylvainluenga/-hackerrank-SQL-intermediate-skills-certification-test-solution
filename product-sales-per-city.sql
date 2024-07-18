fa2a7cf4bd97ec51c305c9c71a99739f879b6602
Alex Sylvain Luenga <38809975+sylvainluenga@users.noreply.github.com>
2024-07-18 14:13:31 +0200

    Update product-sales-per-city.sql

--- product-sales-per-city.sql
+++ product-sales-per-city.sql
@@ -3,3 +3,4 @@ FROM city ci, customer cu, invoice i, invoice_item ii, product pr
 WHERE ci.id = cu.city_id AND cu.id = i.customer_id AND i.id = ii.invoice_id AND ii.product_id = pr.id 
 GROUP BY ci.city_name, pr.product_name 
 ORDER BY tot DESC, ci.city_name, pr.product_name 
+.....
